5b6087cac215f066944597c385afbdcef4f3b90d
vicziani <viczian.istvan@gmail.com>
2024-03-18 16:32:23 +0100

    CRUD kész

--- empapp-web/src/main/java/training/empappweb/EmployeeController.java
+++ empapp-web/src/main/java/training/empappweb/EmployeeController.java
@@ -2,10 +2,8 @@ package training.empappweb;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -27,5 +25,23 @@ public class EmployeeController {
     public Mono<EmployeeResource> findEmployeeById(@PathVariable("id") long id) {
         return employeeService.findEmployeeById(id)
                 .doOnNext(e -> log.debug("Find employee by id: {}, employee: {}", id, e));
+    }
+
+    @PostMapping
+    public Mono<EmployeeResource> createEmployee(@RequestBody EmployeeResource employeeResource) {
+        return employeeService.createEmployee(employeeResource);
+    }
+
+    @PutMapping("/{id}")
+    public Mono<EmployeeResource> updateEmployee(@PathVariable("id") long id, @RequestBody EmployeeResource employeeResource) {
+        if (employeeResource.getId() != id) {
+            throw new IllegalArgumentException("Ids not the same");
+        }
+        return employeeService.updateEmployee(employeeResource);
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<Void> deleteEmployee(@PathVariable long id) {
+        return employeeService.deleteEmployee(id);
     }
 }
