4c90eaf6fe0712daa910faac8aef9cadbbc441f8
vicziani <viczian.istvan@gmail.com>
2024-03-18 16:09:47 +0100

    CRUD repo réteg

--- empapp-web/src/main/java/training/empappweb/EmployeeRepository.java
+++ empapp-web/src/main/java/training/empappweb/EmployeeRepository.java
@@ -4,15 +4,21 @@ import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
 public class EmployeeRepository {
 
-    private final List<Employee> employees = List.of(
-            new Employee(1L, "John Doe"),
-            new Employee(2L, "Jack Doe")
-    );
+    private AtomicLong sequence = new AtomicLong();
+
+    private final List<Employee> employees = Collections.synchronizedList(new ArrayList<>(List.of(
+            new Employee(sequence.incrementAndGet(), "John Doe"),
+            new Employee(sequence.incrementAndGet(), "Jack Doe")
+    )));
+
 
     public Flux<Employee> findAll() {
         return Flux.fromIterable(employees);
@@ -22,5 +28,25 @@ public class EmployeeRepository {
         return Flux.fromIterable(employees)
                 .filter(e -> e.getId() == id)
                 .singleOrEmpty();
+    }
+
+    public Mono<Employee> save(Employee employee) {
+        if (employee.getId() == null) {
+            employee.setId(sequence.incrementAndGet());
+            employees.add(employee);
+            return Mono.just(employee);
+        }
+        else {
+            return Flux.fromIterable(employees)
+                    .filter(e -> e.getId() == employee.getId())
+                    .doOnNext(e -> e.setName(employee.getName()))
+                    .singleOrEmpty();
+
+        }
+    }
+
+    public Mono<Void> deleteById(long id) {
+        employees.removeIf(e -> e.getId() == id);
+        return Mono.empty();
     }
 }
