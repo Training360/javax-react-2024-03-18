0ef687083ce45656c848ea822fe9150b92ff5809
vicziani <viczian.istvan@gmail.com>
2024-03-18 15:11:42 +0100

    Kiindulási webes projekt

---
+++ empapp-web/src/main/java/training/empappweb/EmpappWebApplication.java
@@ -0,0 +1,13 @@
+package training.empappweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpappWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpappWebApplication.class, args);
+	}
+
+}
