83d86bbc9a608c1c14a6028ad7842a23bf83e0ac
vicziani <viczian.istvan@gmail.com>
2024-03-18 16:22:32 +0100

    CRUD service réteg

--- empapp-web/src/main/java/training/empappweb/Employee.java
+++ empapp-web/src/main/java/training/empappweb/Employee.java
@@ -13,4 +13,7 @@ public class Employee {
 
     private String name;
 
+    public Employee(String name) {
+        this.name = name;
+    }
 }
--- empapp-web/src/main/java/training/empappweb/EmployeeService.java
+++ empapp-web/src/main/java/training/empappweb/EmployeeService.java
@@ -23,8 +23,38 @@ public class EmployeeService {
                 .map(this::toResource);
     }
 
+    public Mono<EmployeeResource> createEmployee(EmployeeResource employeeResource) {
+        return Mono.just(employeeResource)
+                .map(this::toEntity)
+                .flatMap(employeeRepository::save)
+                .map(this::toResource);
+    }
+
+    public Mono<EmployeeResource> updateEmployee(EmployeeResource employeeResource) {
+//        return Mono.just(employeeResource)
+//                .map(this::toEntity)
+//                .flatMap(employeeRepository::save)
+//                .map(this::toResource);
+
+        // Mi van akkor, ha az employeeresource NEM tartalmazza az ÖSSZES attribútum értéket
+        // Ha lenne több attribútum, mondjuk 5, és csak a name értékét szeretnénk update-elni
+
+        return Mono.just(employeeResource)
+                .flatMap(r -> employeeRepository.findById(employeeResource.getId()))
+                .doOnNext(e -> e.setName(employeeResource.getName()))
+                .map(this::toResource);
+    }
+
+    public Mono<Void> deleteEmployee(long id) {
+        return employeeRepository.deleteById(id);
+    }
+
     // MapStruct
     public EmployeeResource toResource(Employee employee) {
         return new EmployeeResource(employee.getId(), employee.getName());
+    }
+
+    public Employee toEntity(EmployeeResource employeeResource) {
+        return new Employee(employeeResource.getId(), employeeResource.getName());
     }
 }
