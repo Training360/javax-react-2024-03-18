8a3ddb26600c619ae9e2a1528176229b361d35a4
vicziani <viczian.istvan@gmail.com>
2024-03-18 16:49:35 +0100

    HTTP státuszkódok és header-ök, és 404

--- empapp-web/src/main/java/training/empappweb/EmployeeController.java
+++ empapp-web/src/main/java/training/empappweb/EmployeeController.java
@@ -2,10 +2,13 @@ package training.empappweb;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -22,14 +25,19 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
-    public Mono<EmployeeResource> findEmployeeById(@PathVariable("id") long id) {
+    public Mono<ResponseEntity<EmployeeResource>> findEmployeeById(@PathVariable("id") long id) {
         return employeeService.findEmployeeById(id)
-                .doOnNext(e -> log.debug("Find employee by id: {}, employee: {}", id, e));
+                .doOnNext(e -> log.debug("Find employee by id: {}, employee: {}", id, e))
+                .map(ResponseEntity::ok)
+                .defaultIfEmpty(ResponseEntity.notFound().build())
+                ;
     }
 
     @PostMapping
-    public Mono<EmployeeResource> createEmployee(@RequestBody EmployeeResource employeeResource) {
-        return employeeService.createEmployee(employeeResource);
+    public Mono<ResponseEntity<EmployeeResource>> createEmployee(@RequestBody EmployeeResource employeeResource) {
+        return employeeService
+                .createEmployee(employeeResource)
+                .map(r -> ResponseEntity.created(URI.create("/api/employees/%d".formatted(r.getId()))).body(r));
     }
 
     @PutMapping("/{id}")
@@ -41,6 +49,7 @@ public class EmployeeController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> deleteEmployee(@PathVariable long id) {
         return employeeService.deleteEmployee(id);
     }
