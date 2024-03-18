1c970b55c9b8e09ed73ea2f18ab0b55d283a25b3
vicziani <viczian.istvan@gmail.com>
2024-03-18 15:44:58 +0100

    Repo réteg kész

--- empapp-web/src/main/java/training/empappweb/EmployeeController.java
+++ empapp-web/src/main/java/training/empappweb/EmployeeController.java
@@ -1,6 +1,7 @@
 package training.empappweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -16,5 +17,10 @@ public class EmployeeController {
                 new EmployeeResource(1L, "John Doe"),
                 new EmployeeResource(2L, "Jane Doe")
         );
+    }
+
+    @GetMapping("/{id}")
+    public Mono<EmployeeResource> findEmployeeById(@PathVariable("id") long id) {
+        return Mono.just(new EmployeeResource(id, "John Doe"));
     }
 }
---
+++ empapp-web/src/main/java/training/empappweb/EmployeeRepository.java
@@ -0,0 +1,24 @@
+package training.empappweb;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+public class EmployeeRepository {
+
+    private final List<Employee> employees = List.of(
+            new Employee(1L, "John Doe"),
+            new Employee(2L, "Jack Doe")
+    );
+
+    public Flux<Employee> findAll() {
+        return Flux.fromIterable(employees);
+    }
+
+    public Mono<Employee> findById(long id) {
+        return Flux.fromIterable(employees)
+                .filter(e -> e.getId() == id)
+                .singleOrEmpty();
+    }
+}
