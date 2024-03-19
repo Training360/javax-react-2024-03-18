f420f1e2dedb4b1d647c138b9403cf6183b6a422
vicziani <viczian.istvan@gmail.com>
2024-03-19 09:56:38 +0100

    Validation vége

--- empapp-web/src/main/java/training/empappweb/EmployeesExceptionHandler.java
+++ empapp-web/src/main/java/training/empappweb/EmployeesExceptionHandler.java
@@ -7,14 +7,29 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.util.UUID;
+
 @ControllerAdvice
 public class EmployeesExceptionHandler {
 
     @ExceptionHandler
-    public Mono<ProblemDetail> handle(WebExchangeBindException e) {
+    public Mono<ProblemDetail> handle(WebExchangeBindException exception) {
         var problemDetail = ProblemDetail.forStatusAndDetail(
-                HttpStatus.BAD_REQUEST, e.getMessage()
+                HttpStatus.BAD_REQUEST, exception.getMessage()
         );
+        problemDetail.setType(URI.create("employees/validation-error"));
+        problemDetail.setTitle("Validation error");
+        problemDetail.setProperty("id", UUID.randomUUID().toString());
+
+        var items = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(e -> new ValidationItem(e.getField(), e.getDefaultMessage()))
+                .toList();
+
+        problemDetail.setProperty("validationErrors", items);
+
         return Mono.just(problemDetail);
     }
 }
