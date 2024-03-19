7c77b3cbb872ead8bbb3373b266dbd7413c803a9
vicziani <viczian.istvan@gmail.com>
2024-03-19 12:09:28 +0100

    R2DBC

--- empapp-web-r2dbc/src/main/java/training/empappweb/EmployeeRepository.java
+++ empapp-web-r2dbc/src/main/java/training/empappweb/EmployeeRepository.java
@@ -1,57 +1,10 @@
 package training.empappweb;
 
-import org.springframework.stereotype.Repository;
-import reactor.core.publisher.Flux;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
+public interface EmployeeRepository extends ReactiveCrudRepository<Employee, Long> {
 
-@Repository
-public class EmployeeRepository {
-
-    private AtomicLong sequence = new AtomicLong();
-
-    private final List<Employee> employees = Collections.synchronizedList(new ArrayList<>(List.of(
-            new Employee(sequence.incrementAndGet(), "John Doe"),
-            new Employee(sequence.incrementAndGet(), "Jack Doe")
-    )));
-
-
-    public Flux<Employee> findAll() {
-        return Flux.fromIterable(employees);
-    }
-
-    public Mono<Employee> findById(long id) {
-        return Flux.fromIterable(employees)
-                .filter(e -> e.getId() == id)
-                .singleOrEmpty();
-    }
-
-    public Mono<Employee> save(Employee employee) {
-        if (employee.getId() == null) {
-            employee.setId(sequence.incrementAndGet());
-            employees.add(employee);
-            return Mono.just(employee);
-        }
-        else {
-            return Flux.fromIterable(employees)
-                    .filter(e -> e.getId() == employee.getId())
-                    .doOnNext(e -> e.setName(employee.getName()))
-                    .singleOrEmpty();
-
-        }
-    }
-
-    public Mono<Boolean> existsByName(String name) {
-        return Flux.fromIterable(employees)
-                .any(e -> e.getName().equals(name));
-    }
-
-    public Mono<Void> deleteById(long id) {
-        employees.removeIf(e -> e.getId() == id);
-        return Mono.empty();
-    }
+    // SELECT employee.id FROM employee WHERE employee.name = $1 LIMIT 1
+    Mono<Boolean> existsByName(String name);
 }
