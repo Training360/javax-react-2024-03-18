b0bfebe4e78edfc7d0695143223edb7a32b07c5f
vicziani <viczian.istvan@gmail.com>
2024-03-19 15:04:11 +0100

    Kiinduló projekt

---
+++ empapp-web-r2dbc-cache/src/main/java/training/empappweb/Employee.java
@@ -0,0 +1,21 @@
+package training.empappweb;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+}
